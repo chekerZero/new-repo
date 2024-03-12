95c1e8a2b8c44fec06137ea5c11a7aac8cc1bcd6
Tom Bertha <163115921+chekerZero@users.noreply.github.com>
2024-03-12 11:08:31 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -20,3 +20,12 @@
         return product;
     }
 //yes
+
+    public static T Get<T>(this XElement xelem, string attribute, T defaultT = default)
+    {
+        XAttribute a = xelem.Attribute(attribute);
+        return a == null ? defaultT : (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(a.Value);
+    }
+
+    public static IEnumerable<XElement> Elements(this XElement xelement, params string[] names) => xelement.Elements().Where(e => names.Any(n => n == e.Name));
+}
